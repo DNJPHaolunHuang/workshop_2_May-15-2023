21dbba97995c5271ab8af7427b886a46e2291ae9
FelixHuang <ellen87006@gmail.com>
2023-05-17 05:42:10 +0100

    Mock vehicle did-data

--- src/routes/register-did-on-polygon.ts
+++ src/routes/register-did-on-polygon.ts
@@ -8,29 +8,26 @@ logger.level = 'debug';
 export class RegisterDid {
 
     public routes(app): void {
-
+        var registerDidRes;
         app.post('/register-did', async (req, res) => {
-
             try {
                 const did = req.body.did;
                 const privateKey = req.body.privateKey;
+                const serviceEndpoint = req.body.serviceEndpoint;
+                const url = req.body.url;
+                const contractAddress = req.body.contractAddress;
 
-                const registerDidRes = await registerDID(did, privateKey)
+                registerDidRes = await registerDID(did, privateKey,url,contractAddress)
                     .then((response) => {
                         return response;
                     });
-
-                
-
-                // const gasPrice = registerDidRes.data.txnHash.maxFeePerGas;
-                // const gasLimit = registerDidRes.data.txnHash.gasLimit;
-
-                // const gasPriceDecimal = parseInt(gasPrice._hex.substr(2), 16);
-                // const gasLimitDecimal = parseInt(gasLimit._hex.substr(2), 16);
-                // const txnFee = (gasPriceDecimal * gasLimitDecimal / Math.pow(10, 18))
-
+                const gasPrice = registerDidRes.data.txnHash.maxFeePerGas;
+                const gasLimit = registerDidRes.data.txnHash.gasLimit;
+                const gasPriceDecimal = parseInt(gasPrice._hex.substr(2), 16);
+                const gasLimitDecimal = parseInt(gasLimit._hex.substr(2), 16);
+                const txnFee = (gasPriceDecimal * gasLimitDecimal / Math.pow(10, 18))
                 res.status(201).json({ success: registerDidRes.success, 
-                    // data: { gasPrice, gasLimit, TX_Fee: txnFee },
+                     data: { gasPrice, gasLimit, TX_Fee: txnFee },
                      message: registerDidRes.message });
                 logger.debug(
                     `registerDidRes - ${JSON.stringify(registerDidRes)} \n\n\n`
